aa2f5b7fa94d2c9cc99e23adfed6592ded0917c7
vanderleipinto <vandecopinto@gmail.com>
2024-02-11 16:19:00 -0300

    feat: Validate CNPJ in Supplier, install cpf_cnpj Gem

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,16 @@
 class Supplier < ApplicationRecord
   has_one :account
+
+  has_many :parts, dependent: :destroy
+  validates :name, presence: true
+  validates :cnpj, presence: true, uniqueness: true
+
+  validate :cnpj, :validate_cnpj
+
+  private 
+
+  def validate_cnpj
+     errors.add(:cnpj, "invalid cnpj") unless CNPJ.valid?(cnpj)
+  end
+
 end
