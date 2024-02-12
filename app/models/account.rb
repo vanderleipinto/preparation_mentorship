a92f0296ec67c207986d3aa35902f9829a5c1e2b
vanderleipinto <vandecopinto@gmail.com>
2024-02-12 16:02:11 -0300

    feat: Calculate Verify digit in Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,22 @@
-class Account < ApplicationRecord  
+class Account < ApplicationRecord
   belongs_to :supplier
+
+  validates :account_number, presence: true
+  validate :verify_digit, :calculate_verify_digit
+
+  def calculate_verify_digit
+    number = self.account_number
+    weight = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5]
+    sum = 0
+
+    number.to_s.reverse.chars.each_with_index do |value, index|
+      sum += value.to_i * weight[index % 12]
+    end
+      
+    rest = sum % 11
+    verify_digit = (rest < 2) ? 0 : (11 - rest)
+
+    self.verify_digit = verify_digit.to_s
+    verify_digit
+  end
 end
