68a4e4a9ce112b63a174310e1c838ef39bf33c8d
vanderleipinto <vandecopinto@gmail.com>
2024-02-12 11:43:17 -0300

    feat: Validate CPF in Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,15 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  validates :name, presence: true
+  validates :cpf, presence: true, uniqueness: true
+
+  validate :cpf, :validate_cpf
+
+  private
+
+  def validate_cpf
+    errors.add :cpf, "is invalid" unless CPF.valid?(cpf)
+  end
+  
 end
