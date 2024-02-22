fbe51602e1e081242018c25ef79a7fab38727e4e
vanderleipinto <vandecopinto@gmail.com>
2024-02-22 16:29:27 -0300

    feat: Create Report Supplier

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :authors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get'/authors/:id/report', to: 'authors#report', as: 'authors_report'
+  get'/suppliers/:id/report', to: 'suppliers#report', as: 'suppliers_report'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
